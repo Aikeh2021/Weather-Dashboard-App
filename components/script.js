27fea9e0821e1d813e32e1b23b6b26e2df36e9d4
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-10 21:33:56 -0400

    Formatted my js file, commented in the 5 steps I need for dynamic element generation, and will now begin adding an event listener

--- components/script.js
+++ components/script.js
@@ -1,1 +1,28 @@
 console.log("hello world!")
+$(document).ready(function(){
+
+//DOM Variables
+var cityInput = $("#city-input");
+var ulElement = $("#ul-element");
+var cityDiv = $("#city-date-div");
+var fiveDayDiv = $("#5-day-forecast-cards-div")
+
+
+
+
+//JS Variables
+
+
+
+//Functions
+
+
+
+//Function Calls
+
+
+
+//Event Listeners
+$()
+
+});
