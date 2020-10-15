fd42dea23f3a304f4c9833d8f74b498ccf21d738
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-14 21:56:01 -0400

    Finished adding all of the weather icons for the 5 day forecast cards.

--- components/script.js
+++ components/script.js
@@ -79,8 +79,8 @@ $("#submit-btn").on("click", function(){
       var divHeader = $("#five-day").append("<h3>5-Day-Forecast:</h3>");
       var header1 = $("<h6>").addClass("card-header").text(moment().add(1, 'days').calendar('L'));
       var temp1 = $("<p>").addClass("card-text").text("Temp: " + response.list[6].main.temp + " °F");
-      var weatherIcons = response.list[6].weather[0].icon;
-      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons + ".png"
+      var weatherIcons1 = response.list[6].weather[0].icon;
+      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons1 + ".png"
       var weatherImage1 = $("<img>").attr("src", weatherIconsURL).attr("alt", "weather icon");
       var weatherImageDiv1 = $("<div>").append(weatherImage1);
       var humidity1 = $("<p>").addClass("card-text").text("Humidity: " + response.list[6].main.humidity + "%");
@@ -90,30 +90,46 @@ $("#submit-btn").on("click", function(){
       $("#five-day").append(holderDiv);
       var header2 = $("<h6>").addClass("card-header").text(moment().add(2, 'days').calendar('L'));
       var temp2 = $("<p>").addClass("card-text").text("Temp: " + response.list[14].main.temp + " °F");
+      var weatherIcons2 = response.list[14].weather[0].icon;
+      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons2 + ".png"
+      var weatherImage2 = $("<img>").attr("src", weatherIconsURL).attr("alt", "weather icon");
+      var weatherImageDiv2 = $("<div>").append(weatherImage2);
       var humidity2 = $("<p>").addClass("card-text").text("Humidity: " + response.list[14].main.humidity + "%");
       var newDiv2 = $("<div>").addClass("card text-white bg-primary mb-3 col-lg-2").attr("max-width", "18 rem");
-      newDiv2.append(header2, temp2, humidity2);
+      newDiv2.append(header2, weatherImageDiv2, temp2, humidity2);
       holderDiv.append(newDiv2)
       $("#five-day").append(holderDiv);
       var header3 = $("<h6>").addClass("card-header").text(moment().add(3, 'days').calendar('L'));
       var temp3 = $("<p>").addClass("card-text").text("Temp: " + response.list[22].main.temp + " °F");
+      var weatherIcons3 = response.list[22].weather[0].icon;
+      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons3 + ".png"
+      var weatherImage3 = $("<img>").attr("src", weatherIconsURL).attr("alt", "weather icon");
+      var weatherImageDiv3 = $("<div>").append(weatherImage3);
       var humidity3 = $("<p>").addClass("card-text").text("Humidity: " + response.list[22].main.humidity + "%");
       var newDiv3 = $("<div>").addClass("card text-white bg-primary mb-3 col-lg-2").attr("max-width", "18 rem");
-      newDiv3.append(header3, temp3, humidity3);
+      newDiv3.append(header3, weatherImageDiv3, temp3, humidity3);
       holderDiv.append(newDiv3)
       $("#five-day").append(holderDiv);
       var header4 = $("<h6>").addClass("card-header").text(moment().add(4, 'days').calendar('L'));
       var temp4 = $("<p>").addClass("card-text").text("Temp: " + response.list[30].main.temp + " °F");
+      var weatherIcons4 = response.list[30].weather[0].icon;
+      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons4 + ".png"
+      var weatherImage4 = $("<img>").attr("src", weatherIconsURL).attr("alt", "weather icon");
+      var weatherImageDiv4 = $("<div>").append(weatherImage4);
       var humidity4 = $("<p>").addClass("card-text").text("Humidity: " + response.list[30].main.humidity + "%");
       var newDiv4 = $("<div>").addClass("card text-white bg-primary mb-3 col-lg-2").attr("max-width", "18 rem");
-      newDiv4.append(header4, temp4, humidity4);
+      newDiv4.append(header4, weatherImageDiv4, temp4, humidity4);
       holderDiv.append(newDiv4)
       $("#five-day").append(holderDiv);
       var header5 = $("<h6>").addClass("card-header").text(moment().add(5, 'days').calendar('L'));
       var temp5 = $("<p>").addClass("card-text").text("Temp: " + response.list[38].main.temp + " °F");
+      var weatherIcons5 = response.list[38].weather[0].icon;
+      weatherIconsURL = "https://openweathermap.org/img/wn/" + weatherIcons5 + ".png"
+      var weatherImage5 = $("<img>").attr("src", weatherIconsURL).attr("alt", "weather icon");
+      var weatherImageDiv5 = $("<div>").append(weatherImage5);
       var humidity5 = $("<p>").addClass("card-text").text("Humidity: " + response.list[38].main.humidity + "%");
       var newDiv5 = $("<div>").addClass("card text-white bg-primary mb-3 col-lg-2").attr("max-width", "18 rem");
-      newDiv5.append(header5, temp5, humidity5);
+      newDiv5.append(header5, weatherImageDiv5, temp5, humidity5);
       holderDiv.append(newDiv5)
       $("#five-day").append(holderDiv);
       
